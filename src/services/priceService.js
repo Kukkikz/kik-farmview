24c4a4955290a2ef8964ebcc43511a3625ab31e0
kikmaster <pranout_kik@hotmail.com>
2021-05-26 11:51:22 +0700

    setup tokens for wallet

--- src/services/priceService.js
+++ src/services/priceService.js
@@ -11,24 +11,38 @@ const getPrice = async (address) => {
         let data = {};
         let id = "";
         if (address in tokens) {
+            if (tokens[address].source == "pancake") {
+                const res = await axios.get(`https://api.pancakeswap.info/api/v2/tokens/${address}`);
+                data = res.data;
+                console.log(data);
+                if (data.data.price) {
+                    coinPriceData.set(address, data.data.price);
+                    return data.data.price;
+                } else {
+                    throw new Error(`Price for ${tokens[address].id} is not found`);
+                }
+                
+            }
+
             id = tokens[address].id;
             const res = await axios.get(
                 `https://api.coingecko.com/api/v3/simple/price?ids=${id}&vs_currencies=usd`
             );
             data = res.data;
-        }
-        if (data[id || address]) {
-            if(address == "0xe9e7cea3dedca5984780bafc599bd69add087d56") {
-                if(data[id || address].usd <= 1.05 && data[id || address].usd >= 0.95){
-                    coinPriceData.set(address, 1);
-                    return 1;
+            if (data[id || address]) {
+                if(tokens[address].stable) {
+                    if(data[id || address].usd <= 1.05 && data[id || address].usd >= 0.95){
+                        coinPriceData.set(address, 1);
+                        return 1;
+                    }
                 }
+                coinPriceData.set(address, data[id || address].usd);
+                return data[id || address].usd;
+            } else {
+                throw new Error(`Price for ${id} is not found`);
             }
-            coinPriceData.set(address, data[id || address].usd);
-            return data[id || address].usd;
-        } else {
-            return 0;
         }
+        
     } catch (e) {
         console.log("Error getting price", e);
     }
--- src/services/walletService.js
+++ src/services/walletService.js
@@ -1,1 +1,9 @@
 const priceService = require('./priceService');
+const web3Service = require('./web3Service');
+
+const test = async () => {
+    console.log('test');
+    
+}
+
+test();
